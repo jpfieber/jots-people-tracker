09ee3ee4c2389f331fe3122719b179600e140697
Joseph Fieber <jpfieber@gmail.com>
2025-05-23 14:28:40 -0500

    Added default avatar

--- src/settings.ts
+++ src/settings.ts
@@ -2,11 +2,13 @@ import { App, PluginSettingTab, Setting, TFolder, TAbstractFile } from 'obsidian
 import PeopleTrackerPlugin from './main';
 
 interface PeopleTrackerSettings {
+    enableAvatars: boolean;
     avatarFolderPath: string;
     peopleFolderPath: string;
 }
 
 const DEFAULT_SETTINGS: PeopleTrackerSettings = {
+    enableAvatars: true,
     avatarFolderPath: '',
     peopleFolderPath: 'Sets/People'
 };
@@ -23,17 +25,36 @@ export class PeopleTrackerSettingTab extends PluginSettingTab {
         containerEl.createEl('h2', { text: 'People Tracker Settings' });
 
         new Setting(containerEl)
-            .setName('Avatar folder path')
-            .setDesc('Path to folder containing avatar images')
-            .addDropdown(dropdown =>
-                dropdown
-                    .addOptions(this.getFolderOptions())
-                    .setValue(this.plugin.settings.avatarFolderPath)
+            .setName('Enable avatars')
+            .setDesc('Show avatars next to people links')
+            .addToggle(toggle =>
+                toggle
+                    .setValue(this.plugin.settings.enableAvatars)
                     .onChange(async (value) => {
-                        this.plugin.settings.avatarFolderPath = value;
+                        this.plugin.settings.enableAvatars = value;
+                        // Force refresh of the settings panel to show/hide avatar path
+                        this.display();
                         await this.plugin.saveSettings();
                     })
             );
+
+        if (this.plugin.settings.enableAvatars) {
+            const avatarSetting = new Setting(containerEl)
+                .setName('Avatar folder path')
+                .setDesc('Path to folder containing avatar images')
+                .addDropdown(dropdown =>
+                    dropdown
+                        .addOptions(this.getFolderOptions())
+                        .setValue(this.plugin.settings.avatarFolderPath)
+                        .onChange(async (value) => {
+                            this.plugin.settings.avatarFolderPath = value;
+                            await this.plugin.saveSettings();
+                        })
+                );
+
+            // Add indentation to the setting
+            avatarSetting.settingEl.style.paddingLeft = '2em';
+        }
 
         new Setting(containerEl)
             .setName('People folder path')
